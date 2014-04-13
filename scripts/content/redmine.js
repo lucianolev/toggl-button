b8aeffe18211a4b9bf54575f8a2fafbaf8499a64
Luciano Leveroni <lucianolev@gmail.com>
2014-04-13 02:15:28 -0300

    Redmine: Add issue number as prefix for time entry items.

--- scripts/content/redmine.js
+++ scripts/content/redmine.js
@@ -12,12 +12,14 @@ togglbutton.render('#content', {}, function (elem) {
     return;
   }
 
+  var issueTitleElem = $('#content h2', elem);
+  var issueNumber = issueTitleElem.innerText.match(/.*(#\d+$)/)[1];
   var projectElem = $('#header h1'),
     issueTitleContextualBarElem = $('.contextual', elem);
 
   var link = togglbutton.createTimerLink({
     className: 'redmine',
-    description: issueSubjectElem.innerText,
+    description: issueNumber + ' | ' + issueSubjectElem.innerText,
     projectName: projectElem.innerText
   });
 
@@ -33,13 +35,15 @@ togglbutton.render('.issues-board', {}, function (elem) {
     var issueCardElem = issuesCardsList[i];
     var issueSubjectElem = $('.name a', issueCardElem);
     var issueCardInfoElem = $('.info', issueCardElem);
+    var issueTitleElem = $('.issue-id strong', issueCardElem);
+    var issueNumber = issueTitleElem.innerText.match(/.*(#\d+$)/)[1];
 
     issueCardInfoElem.removeAttribute('style'); //to clear display:none when there's no assignee
 
     var link = togglbutton.createTimerLink({
       className: 'redmine',
       buttonType: 'minimal',
-      description: issueSubjectElem.innerText,
+      description: issueNumber + ' | ' + issueSubjectElem.innerText,
       projectName: projectElem.innerText
     });
 
